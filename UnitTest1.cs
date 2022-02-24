6a17eb3b9ec93c7cbaaaac4276b02cf02e54f3de
hbhuranisq <hbhurani@sharpquest.com>
2022-02-24 17:16:31 +0530

    initial

---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System;
+
+namespace Tests
+{
+    public class Tests
+    {
+        public static AppiumDriver<IWebElement> appiumDriver;
+        [SetUp]
+        public void Setup()
+        {
+            DriverOptions caps = new AppiumOptions();
+            caps.AddAdditionalCapability(MobileCapabilityType.DeviceName, "emulator-5554");
+            caps.AddAdditionalCapability(MobileCapabilityType.App, "C:\\Users\\Haresh\\AndroidStudioProjects\\MyApplication2\\app\\build\\outputs\\apk\\debug\\app-debug.apk");
+            caps.AddAdditionalCapability("automationName", "UiAutomator2");
+            caps.AddAdditionalCapability(MobileCapabilityType.PlatformName, "Android");
+            appiumDriver = new AndroidDriver<IWebElement>(new Uri("http://localhost:4723/wd/hub"), caps);
+            string path = System.AppContext.BaseDirectory;
+        }
+
+        [Test]
+        public void Test1()
+        {
+           if(appiumDriver.FindElementById("com.example.myapplication:id/textview_first").Displayed)
+            {
+                NUnit.Framework.Assert.Pass();
+            }
+           else
+            {
+                NUnit.Framework.Assert.Fail();
+            }
+        }
+    }
+}
